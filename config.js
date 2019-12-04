75526ea33b72565a0ba43cb4e5e9094d30531c52
SangJoonDev <rhapsody3453@gmail.com>
2019-12-04 17:59:23 +0900

    Blog initialize

---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  url: 'https://SangJoonDev.github.io/',
+  title: 'SangJoonDevLand',
+  subtitle: 'HiHelloWorld',
+  copyright: '',
+  disqusShortname: 'github-io-12',
+  postsPerPage: 6,
+  googleAnalyticsId: 'UA-139726433-1',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    }
+    /*
+    ,
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+    */
+  ],
+  author: {
+    name: 'SangJoon',
+    photo: '/profile.png',
+    bio: '',
+    contacts: {
+      email: 'rhapsody3453@gmail.com',
+      github: 'SangJoonDev',
+      // linkedIn: 'sangjoon-park--0830'
+    }
+  }
+};
